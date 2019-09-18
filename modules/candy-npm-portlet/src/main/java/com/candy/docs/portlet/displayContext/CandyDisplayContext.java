d2357d3737339bbe40383b88736e3f3070ececf6
samansum <samansum3@gmail.com>
2019-09-18 21:57:00 +0700

    improve logic

--- modules/candy-npm-portlet/src/main/java/com/candy/docs/portlet/displayContext/CandyDisplayContext.java
+++ modules/candy-npm-portlet/src/main/java/com/candy/docs/portlet/displayContext/CandyDisplayContext.java
@@ -2,25 +2,27 @@ package com.candy.docs.portlet.displayContext;
 
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
-
+import com.liferay.portal.kernel.util.StringPool;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CandyDisplayContext {
-    public String getDataAsJson() {
-        String[] CandyData = new String[]{
-                "01%Candy%hello world",
-                "02%Choco%hello Choco",
-                "03%Hatoya%hello Toya"
-        };
-        List<JSONObject> jsonObjectsData = new ArrayList<>();
-        for (String data : CandyData) {
-            JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-            jsonObject.put("id", data.split("%")[0]);
-            jsonObject.put("name", data.split("%")[1]);
-            jsonObject.put("text", data.split("%")[2]);
-            jsonObjectsData.add(jsonObject);
-        }
-        return jsonObjectsData.toString();
+
+  public String getDataAsJson () {
+    String[] CandyData = new String[]{
+        "01%Candy%hello world",
+        "02%Choco%hello Choco",
+        "03%Hatoya%hello Toya"
+    };
+    List<JSONObject> jsonObjectsData = new ArrayList<>();
+    for (String data : CandyData) {
+      JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+      String[] entry = data.split(StringPool.PERCENT);
+      jsonObject.put("id", entry[0]);
+      jsonObject.put("name", entry[1]);
+      jsonObject.put("text", entry[2]);
+      jsonObjectsData.add(jsonObject);
     }
+    return jsonObjectsData.toString();
+  }
 }
